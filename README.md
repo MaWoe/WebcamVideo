4006798b0c87ec44d78fd1bcf2b246272a8e03fc
Markus Wößner <markus@haus-woessner.de>
2017-01-23 21:58:51 +0100

    Made `bin/makeVideoLoop.sh` parameterized

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # WebcamVideo
 A set of Linux BASH scripts that periodically check a URL that you have configured for new images and melts them together into a video file.
+
+## How to use
+
+# Periodically invoke `bin/loadNewImageAndMakeVideo.sh <url> <imagesDir> <outFile>` via crontab
+# Start a screen and run `makeVideoLoop.sh` with the necessary args
+
--- bin/makeVideoLoop.sh
+++ bin/makeVideoLoop.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+usage() {
+  echo
+  echo "Checks for the \"newImages.txt\" token in given sub dirs"
+  echo "and starts creating video files in the specified out dir."
+  echo
+  echo "Usage: $0 <imageBaseDir> \"<space separated list of sub dirs>\" <out dir>"
+  echo
+  echo "Example: $0 /var/www/WebcamVideo/images \"cam1 cam2 cam3\""
+  echo
+}
+
+if [ -z $1 ] || [ -z $2 ] || [ -z $3 ]; then
+  usage
+  exit 1
+fi
+
 BIN=$(dirname $0)
 BASE=$(realpath "$BIN/..")
-IMG_BASE="$BASE/images"
-WEB="$BASE/web";
+IMG_BASE=$1
+SUB_DIRS="$2"
+OUT_BASE=$3
 
 echo
 echo "IMG_BASE: $IMG_BASE"
@@ -16,10 +33,10 @@ date
 echo "----------------"
 echo "Start scanning"
 
-for i in holz halde schau;do
+for i in $SUB_DIRS;do
 
   IMG="$IMG_BASE/$i"
-  OUT="$WEB/$i.mp4"
+  OUT="$OUT_BASE/$i.mp4"
 
   echo
   echo "IMG: $IMG"
@@ -36,6 +53,7 @@ for i in holz halde schau;do
 
 done
 
+echo
 echo "Sleeping ..."
 sleep 5
 
